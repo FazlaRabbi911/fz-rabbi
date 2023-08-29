d12ab5b8ac3135c623079ca3d5e986ffc8f2060e
FazlaRabbi911 <126706860+FazlaRabbi911@users.noreply.github.com>
2023-08-29 15:34:31 +0600

    my project
    01010

---
+++ js/script.js
@@ -0,0 +1,60 @@
+
+    tailwind.config = {
+        theme: {
+            extend: {
+                fontFamily: {
+                    DM: ["DM Sans"],
+                    Chivo: ["Chivo"],
+                    Inter: ["Inter"],
+                    Poppins: ["Poppins"],
+                },
+                fontSize: {
+                    56: "56px",
+                    46: "46px",
+                    44:"44px",
+                    22: "22px",
+                    30:"30px",
+                    20:"20px"
+
+                },
+                colors: {
+                    c_one: '#3639A4',
+                    c_two: '#43E7DF',
+                    tc_one:'#B4B6C4',
+                    Tc_bold: '#2C2643',
+                    star:'#FDB400'
+                },
+                spacing: {
+                    98: "98px",
+                    96:"96px",
+                    116: "116px",
+                    26: "26px",
+                    30:"30",
+                    36:"36px",
+                    40:"40",
+                    200: "200px",
+                    162: "162px",
+                    55: "55px",
+                    56: "56px",
+                    80:"80px",
+                    50:"50px",
+                    64:"64px",
+                    126: "126px",
+                    140: "140px",
+                    109: "109px",
+                    195:"195px",
+                    291: "291px",
+                    300:"300px"
+                },
+                maxWidth: {
+                    container: '1410px',
+                    796:"796px",
+                    467:"467px"
+                  },
+                minWidth:{
+                    480:"480px",
+                    320:"320px"
+                }
+            }
+        }
+    }
